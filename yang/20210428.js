0b0af316c96a117ded30f31f9aa32eaded051580
원키 <didgur0601@gmail.com>
2021-05-06 17:07:52 +0900

    code runner를 이용한 답 확인

--- yang/20210428.js
+++ yang/20210428.js
@@ -38,5 +38,16 @@ function solution(n, edge) {
       }
     }
   }
+  console.log({ answer });
   return answer;
 }
+
+solution(6, [
+  [3, 6],
+  [4, 3],
+  [3, 2],
+  [1, 3],
+  [1, 2],
+  [2, 4],
+  [5, 2],
+]);
