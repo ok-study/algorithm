c772ad4a06857a685376b99650910330bea30885
m0su-ok <myeongsu.seo@onekey.kr>
2021-06-16 13:43:15 +0900

    2차

--- m0su/algorithm.playground/Contents.swift
+++ m0su/algorithm.playground/Contents.swift
@@ -4,29 +4,33 @@ var list: [String] = []
 
 import Foundation
 
+// MARK: 나의 존재
+
 func solution(_ numbers: [Int]) -> String {
-  permutate(0, array: numbers)
-  print("list: \(list)")
-  let max = list.max() ?? ""
+  let list = numbers.sorted { Int("\($0)\($1)")! > Int("\($1)\($0)")! }
   
-  return max
+  if list[0] == 0 { return "0" }
+  
+  return list.reduce("") { $0 + "\($1)" }
 }
 
-func permutate(_ index: Int, array: [Int]) {
-  if index == array.count {
-    list.append(array.map { "\($0)" }.joined())
-    return
-  }
-  
-  var array = array
-  
-  for i in index..<array.count {
-    array.swapAt(index, i)
-    permutate(index + 1, array: array)
-    array.swapAt(index, i)
-  }
-}
 let item = solution(array)
 
 print("answer: \(item)")
+
+// MARK: 1위
+
+func solution2(_ numbers: [Int]) -> String {
+  let sortedNumbers = numbers.sorted {
+    Int("\($0)\($1)")! > Int("\($1)\($0)")!
+  }
+  
+  let answer = sortedNumbers.map { String($0) }.reduce("") { $0 + $1 }
+  return sortedNumbers.first == 0 ? "0" : answer
+}
+
+// 다른 코드들
+// 정렬된 배열을 String으로 타입 컨버팅을 먼저 함
+// sort 이후 if가 아닌 filter를 통해 0을 캐치한 후 joiend를 이용함: string 배열을 합치는 함수
+// .orderedAscending 옵션을 이용해 sort
 
