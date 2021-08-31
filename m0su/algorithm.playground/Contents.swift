cba920010e49b5bc21cdd15827189b63513ca03c
m0su <sirm0su@gmail.com>
2021-08-31 19:13:08 +0900

    1안

--- m0su/algorithm.playground/Contents.swift
+++ m0su/algorithm.playground/Contents.swift
@@ -1,36 +1,26 @@
-var array = [3, 30, 34, 5, 9]
+//놀이기구의 이용료 price : 1 ≤ price ≤ 2,500, price는 자연수
+//처음 가지고 있던 금액 money : 1 ≤ money ≤ 1,000,000,000, money는 자연수
+//놀이기구의 이용 횟수 count : 1 ≤ count ≤ 2,500, count는 자연수
 
-var list: [String] = []
+// price가 3인 놀이기구를 4번 타고 싶은 고객이 가진 현금이 20이면
+// (3*1) + (3*2) + (3*3) + (3*4) = 30
+// 30-20은 10
 
 import Foundation
 
-// MARK: 나의 존재
-
-func solution(_ numbers: [Int]) -> String {
-  let list = numbers.sorted { Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+func solution(_ price: Int, _ money: Int, _ count: Int) -> Int {
+  var answer: Int = 0
   
-  if list[0] == 0 { return "0" }
-  
-  return list.reduce("") { $0 + "\($1)" }
-}
-
-let item = solution(array)
-
-print("answer: \(item)")
-
-// MARK: 1위
-
-func solution2(_ numbers: [Int]) -> String {
-  let sortedNumbers = numbers.sorted {
-    Int("\($0)\($1)")! > Int("\($1)\($0)")!
+  for index in 1...count {
+    answer += price * index
   }
   
-  let answer = sortedNumbers.map { String($0) }.reduce("") { $0 + $1 }
-  return sortedNumbers.first == 0 ? "0" : answer
+  answer -= money
+  
+  if answer < 0 { answer = 0 }
+  
+  return answer
 }
 
-// 다른 코드들
-// 정렬된 배열을 String으로 타입 컨버팅을 먼저 함
-// sort 이후 if가 아닌 filter를 통해 0을 캐치한 후 joiend를 이용함: string 배열을 합치는 함수
-// .orderedAscending 옵션을 이용해 sort
 
+print("result: \(solution(3, 20, 4))")
